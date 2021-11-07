8ecacbd7f3eaca12868587873d1b733a269ab84c
Markus Fleischmann <oldrover@web.de>
2021-11-07 18:34:47 +0100

    created EventForm

---
+++ src/components/Forms/EventForm.js
@@ -0,0 +1,11 @@
+export const EventForm = (props) => {
+    
+    return (
+        <div className="EventForm">
+            test
+            <button onClick={props.handleClose}>
+                Close
+            </button>
+        </div>
+    )
+}
