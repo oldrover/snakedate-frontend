de84a010a7125ada71d59262d9a19fa95f1e96aa
Markus Fleischmann <oldrover@web.de>
2022-04-06 12:52:44 +0200

    added mobilefriedly menu

--- src/components/Menu/Menu/Menu.js
+++ src/components/Menu/Menu/Menu.js
@@ -1,7 +1,11 @@
+import { useState } from 'react';
+
 import './Menu.css';
 import { ChangeSnake } from '../ChangeSnake/ChangeSnake';
 import { MenuItem } from '../MenuItem/MenuItem';
 import { menuItems } from '../MenuStrucuture';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
 
 export const Menu = (props) => {
 
@@ -9,6 +13,14 @@ export const Menu = (props) => {
     const snake = props.snake;
     const setSnake = props.setSnake;
     const handleShowForm = props.handleShowForm;
+
+    const [showMenu, setShowMenu] = useState(false);
+
+    
+    const showCollapsedMenu = () => {
+        showMenu ? setShowMenu(false) : setShowMenu(true); 
+        
+    }
 
     return(
         <div className='menu'>
@@ -18,17 +30,26 @@ export const Menu = (props) => {
                 setSnake={setSnake}
                 handleShowForm={handleShowForm}
             />
-            {
-                menuItems.map(item => {
-                    return(                        
-                        <MenuItem                             
-                            item={item}
-                            key={item.name}
-                        />
-                    )
-                })
-                
-            }
+            <div className='menu_collapsed'
+                onClick={showCollapsedMenu}
+            >
+                <FontAwesomeIcon icon={(showMenu && faMinus) || faPlus}/>
+            </div>
+            { 
+                <div className={(showMenu && 'menu_items' )|| 'menu_items is_collapsed'}>
+                {               
+                    menuItems.map(item => {
+                        return(                        
+                            <MenuItem                             
+                                item={item}
+                                key={item.name}
+                            />
+                        )
+                    })                
+                }
+                </div> 
+                           
+            }      
             
         </div>
     )
